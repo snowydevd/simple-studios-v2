db874c2d3832b7e86302085d0e7e3b3ee1b0d48c
Snowie Dev <lautarochini4@gmail.com>
2023-10-02 12:40:12 -0300

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,21 @@
-
-
-import './globals.css'
-import Navbar from './components/Navbar'
-
+import { Analytics } from "@vercel/analytics/react";
+import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata = {
-  title: 'Simple Studios',
-  description: 'Webpage Creation Services',
-  
-}
+  title: "Simple Studios",
+  description: "Webpage Creation Services",
+};
 
 export default function RootLayout({ children }) {
   return (
-   
-
     <html lang="en-es">
-      <body >
-            <Navbar/>
-            {children}
+      <body>
+        <Navbar />
+        {children}
+        <Analytics />
         {/* <script src="../path/to/flowbite/dist/flowbite.min.js"></script> */}
       </body>
     </html>
-
-   
-      
-   
-  )
+  );
 }
