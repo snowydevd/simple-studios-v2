e587c3975dcbb73645d2c86f1696deb4b9dcd2ed
Snowie Dev <lautarochini4@gmail.com>
2023-10-10 17:04:42 -0300

    link fix

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function SimpleButton({ children, href }) {
   return (
-    <a
+    <Link
       href={href}
       className="px-5 py-2 text-xl  tracking-tight rounded-lg bg-slate-800 hover:bg-blue-800 hover:-translate-y-1 duration-200 text-white"
     >
       {children}
-    </a>
+    </Link>
   );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,52 +1,53 @@
 import { AiOutlineMenu } from "react-icons/ai";
 import Image from "next/image";
+import Link from "next/link";
 import simpleLogo from "../../public/logo.svg";
 
 export default async function Navbar() {
   return (
     <nav className=" flex flex-row justify-between items-center p-10 sticky top-0 bg-black z-50  rounded-lg">
-      <a
+      <Link
         href="/"
         className="text-3xl font-extrabold tracking-tighter text-white"
       >
-        Simple Studios
-      </a>
+        Simple Studios ®
+      </Link>
 
       {/* <Image alt='' width={125} src={simpleLogo}/> */}
       <div className="hidden lg:block ">
         <div className="flex gap-12 ">
           <ul className="flex gap-7 justify-center items-center text-xl font-semibold ">
             <li className="hover:-translate-y-1 duration-200">
-              <a
+              <Link
                 href="/plans"
                 className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
               >
                 Planes
-              </a>
+              </Link>
             </li>
             <li className="hover:-translate-y-1 duration-200">
-              <a
+              <Link
                 href="/info"
                 className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
               >
                 Informacion
-              </a>
+              </Link>
             </li>
             <li className="hover:-translate-y-1 duration-200">
-              <a
+              <Link
                 href="/affiliates"
                 className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
               >
                 Afiliados
-              </a>
+              </Link>
             </li>
             <li className="hover:-translate-y-1 duration-200">
-              <a
+              <Link
                 href="/contact"
                 className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
               >
                 Contacto
-              </a>
+              </Link>
             </li>
           </ul>
 
@@ -69,43 +70,48 @@ export default async function Navbar() {
             </div>
             <div className="drawer-side l">
               <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-              <div className="menu  flex min-h-full bg-base-200 text-base-content  justify-start items-center">
-                <Image alt="drawer logo" src={simpleLogo} width={100} />
+              <div className="menu  flex min-h-full bg-base-200 text-base-content justify-between items-center">
                 <ul className="menu p-4 w-56 h-72 flex text-base-content text-xl flex-col font-semibold">
+                  {/* <Image alt="drawer logo" src={simpleLogo} width={100} /> */}
                   {/* Sidebar content here */}
                   <li className="hover:-translate-y-1 duration-200">
-                    <a
+                    <Link
                       href="/plans"
                       className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
                     >
                       Planes
-                    </a>
+                    </Link>
                   </li>
                   <li className="hover:-translate-y-1 duration-200">
-                    <a
+                    <Link
                       href="/info"
                       className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
                     >
                       Informacion
-                    </a>
+                    </Link>
                   </li>
                   <li className="hover:-translate-y-1 duration-200">
-                    <a
+                    <Link
                       href="/affiliates"
                       className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
                     >
                       Afiliados
-                    </a>
+                    </Link>
                   </li>
                   <li className="hover:-translate-y-1 duration-200">
-                    <a
+                    <Link
                       href="/contact"
                       className="hover:shadow-lg hover:bg-slate-950 hover:shadow-slate-700 text-white px-3 py-2 rounded-lg duration-150"
                     >
                       Contacto
-                    </a>
+                    </Link>
                   </li>
                 </ul>
+
+                <div className="w-full border-t-2 border-slate-300 rounded-lg font-light text-xs text-center text-slate-50">
+                  <h2>Simple Studios ©</h2>
+                  <h2>Todos los derechos reservados</h2>
+                </div>
               </div>
             </div>
           </div>
